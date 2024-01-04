c0f2e81b33a5299634a041b49e55f44b1e9ffd22
savan <savannahwilliams243@gmail.com>
2024-01-04 17:47:52 -0500

    syncing sequelize w db

--- server.js
+++ server.js
@@ -38,3 +38,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
+
+// sync sequelize with database
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+  });
