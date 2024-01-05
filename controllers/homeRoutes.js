0515f56880a8661123a35e2e94c7a03e290da782
savan <savannahwilliams243@gmail.com>
2024-01-05 10:54:46 -0500

    completed get route in homeRoute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,35 @@
 const router = require("express").Router();
 const { BlogPost, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+    try {
+        // get all blogPosts and JOIN with user data and comment data
+        const blogPostData = await BlogPost.findAll({
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+            {
+              model: Comment,
+              attributes: ["comment_body"],
+            },
+          ],
+        });
+    
+        // serialize data so the template can read it
+        const blogPosts = blogPostData.map((blogPost) =>
+          blogPost.get({ plain: true })
+        );
+    
+        // pass serialized data and session flag into template
+        res.render("homepage", {
+          blogPosts,
+          logged_in: req.session.logged_in,
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    });
