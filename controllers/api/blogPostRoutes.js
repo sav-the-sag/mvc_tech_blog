4b0ba748e8227abbffa7a0f8d02334b245d9ae65
savan <savannahwilliams243@gmail.com>
2024-01-05 11:05:39 -0500

    added route to create new blog post with router.post

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -2,3 +2,19 @@
 const router = require("express").Router();
 const { BlogPost } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+// route to create a new blog post
+router.post("/", withAuth, async (req, res) => {
+    console.log(req.body);
+    try {
+      const newBlogPost = await BlogPost.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newBlogPost);
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err);
+    }
+  });
